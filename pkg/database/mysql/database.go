a05b75f16455636a99114c8c83b2121012d9a81f
Luke <rlwjd1504@gmail.com>
2023-05-20 18:17:43 +0900

    feat: add mysql client

---
+++ pkg/database/mysql/database.go
@@ -0,0 +1,24 @@
+package mysql
+
+import (
+	"github.com/copolio/namegpt/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+var database *gorm.DB
+
+func InitConnection() {
+	conf := config.Get()
+	dsn := conf.DSN
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting database: " + err.Error())
+	}
+	database = db
+}
+
+func Get() *gorm.DB {
+	return database
+}
